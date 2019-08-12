679a5c7d372ffd9e3dc6173df1d26ec078ab4e2e
Karolis Ramanauskas <karolisr@gmail.com>
2019-08-12 01:25:07 -0500

    BugFix in write_genbank_records: correctly handle situations where a single record is to be written.

--- cs_green_utils/bio_io.py
+++ cs_green_utils/bio_io.py
@@ -2,6 +2,9 @@
 """Read / Write sequence files."""
 
 def write_genbank_records(gb_records, file):  # noqa
-    gb_records_string = '\n\n'.join(gb_records)
+    if type(gb_records) in (list, tuple):
+        gb_records_string = '\n\n'.join(gb_records)
+    else:
+        gb_records_string = gb_records
     with open(file, 'w') as f:
         f.write(gb_records_string)
